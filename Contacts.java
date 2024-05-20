f3e8c39a2a0f34c38f5dcd105ee59cededaba6ab
Adithya Nair <adithyanair@AdiNairs-Macbook-Pro.local>
2024-05-20 09:43:59 -0700

    change

--- Contacts.java
+++ Contacts.java
@@ -25,10 +25,13 @@ public class Contacts{
        this.isEmergency = isEmergency;
        this.company = company;
        contacts.add(this);
+       favorites(isFavorite);
+       emergency(isEmergency);
+
    }
   
    // returns a list of contacts that contains the name
-   public ArrayList<Contacts> searchContact(String name){
+   public static ArrayList<Contacts> searchContact(String name){
        ArrayList<Contacts> contactResults = new ArrayList<Contacts>();
        ArrayList<Contacts> sortedContacts = sortContacts(contacts);
        for(int i = 0; i < sortedContacts.size(); i++){
@@ -39,7 +42,7 @@ public class Contacts{
        return contactResults;
    }
   
-   public ArrayList<Contacts> sortContacts(ArrayList<Contact> unsorted){
+   public static ArrayList<Contacts> sortContacts(ArrayList<Contact> unsorted){
        ArrayList<String> sortedNames = new ArrayList<String>();
        ArrayList<Contacts> sortedContacts = new ArrayList<Contacts>();
        for(int i = 0; i < unsorted.size(); i++){
@@ -52,6 +55,17 @@ public class Contacts{
        return sortedContacts;
    }
 
+   public void favorites(boolean isFavorite){
+        if(isFavorite){
+            addToFavorites();
+        }
+   }
+
+   public void emergency(boolean isEmergency){
+        if(isEmergency){
+            addToEmergency();
+        }
+   }
 
    public void addToFavorites(){
        isFavorite = true;
@@ -59,13 +73,11 @@ public class Contacts{
            favorites.add(this);
    }
 
-
    public void addToEmergency(){
        isEmergency = true;
        if (!emergency.contains(this))
            emergency.add(this);
    }
-
 
    public void removeFavorites(){
        isFavorite = false;
@@ -73,27 +85,23 @@ public class Contacts{
            emergency.remove(emergency.indexOf(this));
    }
 
-
    public void removeEmergency(){
        isEmergency = false;
        if (emergency.contains(this))
            emergency.remove(emergency.indexOf(this));
    }
 
-
    public static ArrayList<Contacts> getContacts(){
        return contacts;
    }
 
-
    public static ArrayList<Contacts> getFavorites(){
        return favorites;
    }
-  
+
    public static ArrayList<Contacts> getEmergency(){
        return emergency;
    }
-
 
    public String getFirstName(){
        return firstName;
@@ -105,21 +113,17 @@ public class Contacts{
        return firstName + " " + lastName;
    }
 
-
    public String getPhoneNumber(){
        return phoneNumber;
    }
-
 
    public String getEmail(){
        return email;
    }
 
-
    public String getAddress(){
        return address;
    }
-
 
    public void setFirstName(String newFirstName){
        firstName = newFirstName;
@@ -132,25 +136,23 @@ public class Contacts{
        phoneNumber = newPhoneNumber;
    }
 
-
    public void setEmail(String newEmail){
        email = newEmail;
    }
 
-
    public void setAddress(String newAddress){
        address = newAddress;
    }
-
 
    public static void main(String[] args) {
        Contacts contact1 = new Contacts("Adam", "Appleseed", "9168959021", "adamappleseed@gmail.com", "456 Dinglenut Way", true, true, "Google");
        Contacts contact2 = new Contacts("Billy", "Bobby", "9167248902", "billybobby@gmail.com", "123 Dinglenut Way", false, false, "Google");
        Contacts contact3 = new Contacts("Mark", "Cucumberg", "9162323451", "markbigcucumber@gmail.com", "78 Veggie Drive", false, true, "Facebook");
        Contacts contact4 = new Contacts("Tim", "Cooked", "9169509743", "buyapple@gmail.com", "52 Fruit Drive", true, false, "Apple");
-       System.out.println("All Contacts:"+Contacts.getContacts());
-       System.out.println("Favorites:"+Contacts.getFavorites());
-       System.out.println("Emergency Contacts:"+Contacts.getEmergency());
+       System.out.println("All Contacts:"+getContacts());
+       System.out.println("Favorites:"+getFavorites());
+       System.out.println("Emergency Contacts:"+getEmergency());
+        System.out.println("Search for 'Adam':"+searchContact("Adam"));
    }
 }
 
